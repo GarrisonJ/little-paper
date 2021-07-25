9a91d60f3de11eb4cb1909606842241a356ef827
Garrison Jensen <garrison.jensen@gmail.com>
2021-07-25 14:04:48 -0700

    Update styles on edit page

--- Web/View/Posts/Edit.hs
+++ Web/View/Posts/Edit.hs
@@ -5,18 +5,12 @@ data EditView = EditView { post :: Post }
 
 instance View EditView where
     html EditView { .. } = [hsx|
-        <nav>
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href={PostsAction}>Posts</a></li>
-                <li class="breadcrumb-item active">Edit Post</li>
-            </ol>
-        </nav>
-        <h1>Edit Post</h1>
+        <h1>{get #createdOnDay post}</h1>
         {renderForm post}
     |]
 
 renderForm :: Post -> Html
 renderForm post = formFor post [hsx|
-    {(textareaField #body)}
+    {(textareaField #body) { disableLabel = True, fieldClass="edit-text-area" }}
     {submitButton}
 |]
