f4ce6e95fd2111c20c444dcc8de6d4cb6a5e2cb3
Garrison Jensen <garrison.jensen@gmail.com>
2021-08-01 08:40:06 -0700

    Update new post page styling

--- Web/View/Posts/New.hs
+++ Web/View/Posts/New.hs
@@ -7,18 +7,12 @@ data NewView = NewView { post :: Post,
 
 instance View NewView where
     html NewView { .. } = [hsx|
-        <nav>
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href={PostsAction}>Posts</a></li>
-                <li class="breadcrumb-item active">New Post</li>
-            </ol>
-        </nav>
         <h3>{day}</h3>
         {renderForm post}
     |]
 
 renderForm :: Post -> Html
 renderForm post = formFor post [hsx|
-    {(textField #body) { disableLabel = True }}
+    {(textareaField #body) { disableLabel = True }}
     {submitButton}
 |]
