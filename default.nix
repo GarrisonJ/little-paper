5646f94aa7b775a8393d22d04f313ebe8fb9cd4e
Garrison Jensen <garrison.jensen@gmail.com>
2021-11-14 14:29:23 -0800

    Upgrade IHP version

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
     ihp = builtins.fetchTarball {
-        url = "https://ihp.digitallyinduced.com/BuildTarball?userId=f0a92fe8-8d53-4e8d-9aba-c96e37ce6573&token=okrAbQRohprwDJtzwdrogtOMVvyielRT&version=7d4f6170c7a98cf8625361fe7ffce6d27d9afd56";
-        sha256 = "083s3k6cbd719jij3qy0gcqb4bsaqqibiq00zx29d8wvay28crzm";
+        url = "https://ihp.digitallyinduced.com/BuildTarball?userId=f0a92fe8-8d53-4e8d-9aba-c96e37ce6573&token=okrAbQRohprwDJtzwdrogtOMVvyielRT&version=a00e836ae8b0ebe36ac337fe4c7f845695011aee";
+        sha256 = "0ph261rhix775cq8n90gh0cy94m9068cbczsf2wdfa7j1575z8w6";
     };
     haskellEnv = import "${ihp}/NixSupport/default.nix" {
         ihp = ihp;
