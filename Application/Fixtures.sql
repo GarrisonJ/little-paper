6a9df429d7972bb7bbd6c615b5576e077c8aad36
Garrison Jensen <garrison.jensen@gmail.com>
2023-05-07 16:10:15 -0700

    Reset Fixtures.sql to be empty

--- Application/Fixtures.sql
+++ Application/Fixtures.sql
@@ -23,17 +23,9 @@ ALTER TABLE public.admins ENABLE TRIGGER ALL;
 
 ALTER TABLE public.users DISABLE TRIGGER ALL;
 
-INSERT INTO public.users (id, email, password_hash, locked_at, failed_login_attempts, timezone, username, is_confirmed, picture_url, created_at, confirmation_key, failed_email_confirm_attempts) VALUES ('53e5d539-4c2e-4561-9564-6bf1ff656f2b', 'garrison.jensen@gmail.com', 'sha256|17|tMXBmOZemxAm2AurJvj3Fw==|EAeBs9UDRO4Wm6wW7Sk41sGBBKCEz52iTRPLjx0wJzk=', NULL, 0, 'America/Los_Angeles', 'garrison', true, NULL, '2021-08-03 18:01:33.02564-07', 'sha256|17|NNiBtpxVx06oYuCcTRZpIA==|pT0ALZjFredLyxyko5xm8Da+VgykRSaklxpwlTmhoh0=', 3);
 
 
 ALTER TABLE public.users ENABLE TRIGGER ALL;
-
-
-ALTER TABLE public.password_resets DISABLE TRIGGER ALL;
-
-
-
-ALTER TABLE public.password_resets ENABLE TRIGGER ALL;
 
 
 ALTER TABLE public.posts DISABLE TRIGGER ALL;
@@ -43,9 +35,56 @@ ALTER TABLE public.posts DISABLE TRIGGER ALL;
 ALTER TABLE public.posts ENABLE TRIGGER ALL;
 
 
+ALTER TABLE public.comments DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.comments ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.likes DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.likes ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.login_state DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.login_state ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.notifications DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.notifications ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.password_resets DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.password_resets ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.schema_migrations DISABLE TRIGGER ALL;
+
+INSERT INTO public.schema_migrations (revision) VALUES (1659839098);
+INSERT INTO public.schema_migrations (revision) VALUES (1659843777);
+INSERT INTO public.schema_migrations (revision) VALUES (1668894880);
+INSERT INTO public.schema_migrations (revision) VALUES (1668895038);
+INSERT INTO public.schema_migrations (revision) VALUES (1669408758);
+INSERT INTO public.schema_migrations (revision) VALUES (1669409262);
+
+
+ALTER TABLE public.schema_migrations ENABLE TRIGGER ALL;
+
+
 ALTER TABLE public.user_follows DISABLE TRIGGER ALL;
 
-INSERT INTO public.user_follows (id, follower_id, followed_id) VALUES ('cf77b80a-5e8d-42b6-96b6-c2f3467d2f50', '53e5d539-4c2e-4561-9564-6bf1ff656f2b', '53e5d539-4c2e-4561-9564-6bf1ff656f2b');
 
 
 ALTER TABLE public.user_follows ENABLE TRIGGER ALL;
